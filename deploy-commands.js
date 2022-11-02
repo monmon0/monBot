b8d7d3160593846e4b903cf882c1e6c1d9a827ef
monmon <107778876+monmon0@users.noreply.github.com>
2022-11-02 14:41:58 -0600

    lets try it this way

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,25 @@
-// const { SlashCommandBuilder, Routes } = require('discord.js');
-// const { REST } = require('@discordjs/rest');
-// const { clientId } = process.env.CLIENT_ID;
-// const { guildId } = process.env.GUILD_ID;
-// const { token } = process.env.TOKEN;
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const clientId = process.env.CLIENT_ID;
+const  guildId  = process.env.GUILD_ID;
+const token  = process.env.TOKEN;
 
-// const commands = [
-// 	new SlashCommandBuilder().setName('god').setDescription('God complex affirmation'),
-// 	new SlashCommandBuilder().setName('rekt').setDescription('Wish someone a nice rekt').addUserOption(option => option.setName('target').setDescription('Select a user')),
-// 	new SlashCommandBuilder().setName('wordle').setDescription('Deciding Wordle Champion otd')
-// 	.addIntegerOption(option => option.setName('int').setDescription('Enter single digit wordle tries').setRequired(true)),
-// 	// .addStringOption(option => option.setName('contestant').setDescription('Enter your server username').setRequired(true)),
-// 	// new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-// 	new SlashCommandBuilder().setName('wordlegod').setDescription(' Wordle God OTD ')
-// 	.addStringOption(option => option.setName('date').setDescription('Enter todays date').setRequired(true)),
-// ]
-// 	.map(command => command.toJSON());
+const commands = [
+	new SlashCommandBuilder().setName('god').setDescription('God complex affirmation'),
+	new SlashCommandBuilder().setName('rekt').setDescription('Wish someone a nice rekt').addUserOption(option => option.setName('target').setDescription('Select a user')),
+	new SlashCommandBuilder().setName('wordle').setDescription('Deciding Wordle Champion otd')
+	.addIntegerOption(option => option.setName('int').setDescription('Enter single digit wordle tries').setRequired(true)),
+	// .addStringOption(option => option.setName('contestant').setDescription('Enter your server username').setRequired(true)),
+	// new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('wordlegod').setDescription(' Wordle God OTD ')
+	.addStringOption(option => option.setName('date').setDescription('Enter todays date').setRequired(true)),
+]
+	.map(command => command.toJSON());
 
-// const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-// 	.then(() => console.log('Successfully registered application commands.'))
-// 	.catch(console.error);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
 
-// console.log(token)
+console.log(token)
